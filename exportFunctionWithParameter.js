4000beb4630a27c0402ade408c379d30f0b00ae6
P-Joel-Prakash <135586760+P-Joel-Prakash@users.noreply.github.com>
2023-06-08 18:39:27 +0530

    Add files via upload

---
+++ exportFunctionWithParameter.js
@@ -0,0 +1,9 @@
+// exportFunctionWithParameter.js
+
+// Define the function that accepts a number and returns its square
+function squareNumber(number) {
+  return number * number;
+}
+
+// Export the function using the default export syntax
+module.exports = squareNumber;
---
+++ importFunctionWithParameter.js
@@ -0,0 +1,10 @@
+// importFunctionWithParameter.js
+
+// Import the function from the exportFunctionWithParameter module
+const squareNumber = require("./exportFunctionWithParameter");
+
+// Call the imported function with a number
+const result = squareNumber(5);
+
+// Print the result
+console.log(result);
